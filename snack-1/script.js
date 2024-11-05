d6006ad2a61dce5771a1fd4ea4cfec4dcd06d350
antonioferrigno28 <ferrigno1@hotmail.it>
2024-11-05 15:00:13 +0100

    Snack 1 done!

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,16 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//ELABORAZIONE CICLO FOR
+
+// console.log(names);
+// for (let i = 0; i > names.length; i++) {
+//   console.log(names[i]);
+// }
+
+//ELABORAZIONE FOREACH
+names.forEach((personName) => {
+  console.log(personName);
+});
