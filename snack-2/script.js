10a46d1072c0195e6fedac014e602b94b5c94eee
antonioferrigno28 <ferrigno1@hotmail.it>
2024-11-05 15:17:46 +0100

    Snack 1,2,3 done!

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,15 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//ELABORAZIONE FOREACH
+
+people.forEach((currentPerson) => {
+  console.log(currentPerson.name);
+});
