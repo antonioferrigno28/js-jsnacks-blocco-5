08d0e590d56557fcb3c120af039e01a965c046b4
antonioferrigno28 <ferrigno1@hotmail.it>
2024-11-05 15:34:05 +0100

    Snack 4,5,6,7 done!

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,14 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//ELABORAZIONE FOREACH
+const oddNumbers = [];
+
+nums.forEach((number) => {
+  if (number % 2 === 0) {
+    oddNumbers.push(number);
+  }
+});
+
+console.log(oddNumbers);
--- snack-6/script.js
+++ snack-6/script.js
@@ -1,14 +1,28 @@
 const zucchine = [
-  { type: 'Napoletana', weight: 10, length: 4 },
-  { type: 'Trombetta', weight: 13, length: 16 },
-  { type: 'Napoletana', weight: 4, length: 23 },
-  { type: 'Trombetta', weight: 11, length: 6 },
-  { type: 'Napoletana', weight: 2, length: 17 },
-  { type: 'Romana', weight: 5, length: 10 },
-  { type: 'Romana', weight: 7, length: 9 },
-  { type: 'Trombetta', weight: 3, length: 8 },
-  { type: 'Calabrese', weight: 6, length: 27 },
-  { type: 'Calabrese', weight: 14, length: 4 },
+  { type: "Napoletana", weight: 10, length: 4 },
+  { type: "Trombetta", weight: 13, length: 16 },
+  { type: "Napoletana", weight: 4, length: 23 },
+  { type: "Trombetta", weight: 11, length: 6 },
+  { type: "Napoletana", weight: 2, length: 17 },
+  { type: "Romana", weight: 5, length: 10 },
+  { type: "Romana", weight: 7, length: 9 },
+  { type: "Trombetta", weight: 3, length: 8 },
+  { type: "Calabrese", weight: 6, length: 27 },
+  { type: "Calabrese", weight: 14, length: 4 },
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const longerThanFifteen = zucchine.filter(
+  (currentElement) => currentElement.length > 15
+);
+
+const shorterThanFifteen = zucchine.filter(
+  (currentElement) => currentElement.length < 15
+);
+
+console.log("Array con zucchine più lunghe di 15cm");
+console.log(longerThanFifteen);
+console.log("Array con zucchine più corte di 15 cm");
+console.log(shorterThanFifteen);
